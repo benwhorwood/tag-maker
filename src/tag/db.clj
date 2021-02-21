499779e2acf30bf622a3ea7dca2d77c95985522f
Ben Whorwood <ben@flowmo.co>
2021-02-21 20:46:22 +0000

    Write out hardcoded seed words (animals and actions)

--- src/tag/db.clj
+++ src/tag/db.clj
@@ -2,6 +2,26 @@
   (:require [jsonista.core :as json]
             [tag.fetch :as fetch]))
 
+(def animals
+  ["goat"
+   "donkey"
+   "frog"
+   "mule"
+   "doggy"
+   "llama"
+   "cat"
+   "newt"])
+
+(def actions
+  ["yawning"
+   "poking"
+   "dancing"
+   "smiling"
+   "eating"
+   "jesting"
+   "gigging"])
+
+
 (defn rhyming-words
   "Coll of words related to word"
   [n ^String word]
@@ -17,27 +37,14 @@
          words)))
 
 
+(defn- spit-animals!
+  []
+  (spit "resources/animals.json" (json/write-value-as-string animals)))
+
+(defn- spit-actions!
+  []
+  (spit "resources/actions.json" (json/write-value-as-string actions)))
+
 (defn- spit-animals-rhyming!
   []
   (spit "resources/animals_rhyming.json" (json/write-value-as-string (gen-rhyming 8 animals))))
-
-
-(def animals
-  ["goat"
-   "donkey"
-   "frog"
-   "mule"
-   "doggy"
-   "llama"
-   "cat"
-   "newt"])
-
-
-(def actions
-  ["yawning"
-   "poking"
-   "dancing"
-   "smiling"
-   "eating"
-   "jesting"
-   "gigging"])
